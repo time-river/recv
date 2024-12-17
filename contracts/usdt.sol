93b4d690b7e596b248960cd780c37f6ac654d441
Fu Lin <fulin@yobit.tech>
2024-12-17 10:06:11 +0800

    update

---
+++ contracts/usdt.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "./ITRC20.sol";
+
+contract Account {
+    address public owner;
+    address payable public receiver;
+
+    event Flush(address, uint256);
+
+    constructor(address payable _receiver) {
+        owner = msg.sender;
+        receiver = _receiver;
+    }
+
+    function flush() public {
+        require(msg.sender == owner, "403");
+
+        address current = address(this);
+        uint256 balance = ITRC20(current).balanceOf(current);
+        if (balance == 0) {
+            return;
+        }
+
+        ITRC20(current).transfer(receiver, balance);
+        emit Flush(receiver, balance);
+    }
+}
+
+contract Gateway {
+    address public owner;
+    mapping(address => bool) public accounts;
+
+    event Create(address);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function create(address payable _to, bytes32 _salt) public {
+        require(msg.sender == owner, "403");
+
+        Account account = new Account{salt: _salt}(_to);
+        accounts[address(account)] = true;
+        emit Create(address(account));
+    }
+}
---
+++ scripts/start-tron.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# start tron development network
+docker run -d -p 9090:9090 --rm --name tron tronbox/tre
