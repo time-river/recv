d42fb7513c7608eaa4149c852f5cfdf9fdb12a45
Fu Lin <fulin@yobit.tech>
2024-12-28 21:19:41 +0800

    wallet: init
    Signed-off-by: Fu Lin <fulin@yobit.tech>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,15 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;
--- test/Wallet.ts
+++ test/Wallet.ts
@@ -1,52 +1,112 @@
+import {
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
 import hre from "hardhat";
 import { expect } from "chai";
+import { bigint } from "hardhat/internal/core/params/argumentTypes";
 
 describe("Wallet", () => {
   async function deploy() {
+    const [owner, otherAccount] = await hre.viem.getWalletClients();
 
+    const wallet = await hre.viem.deployContract("Wallet", []);
+    const publicClient = await hre.viem.getPublicClient();
+
+    return {wallet, owner, otherAccount, publicClient};
   }
 
   describe("Deploy", () => {
     it("deploy contract", async () => {
-
+      await loadFixture(deploy);
     });
   });
 
   describe("Access", () => {
     it("get owner", async () => {
+      const { wallet, owner } = await loadFixture(deploy);
 
+      const addr1 = owner.account.address;
+      const addr2 = await wallet.read.owner();
+      expect(addr1.toLocaleLowerCase()).to.equal(addr2.toLocaleLowerCase());
     });
 
     it("allow transfer ownership", async () => {
+      const { wallet, owner, otherAccount } = await loadFixture(deploy);
 
+      const addr1 = await wallet.read.owner();
+      const addr2 = otherAccount.account.address;
+      expect(addr1.toLocaleLowerCase()).to.not.equal(addr2.toLocaleLowerCase());
+
+      await wallet.write.transferOwnership([addr2]);
+      const addr3 = await wallet.read.owner();
+      expect(addr3.toLocaleLowerCase()).to.equal(addr2.toLocaleLowerCase());
     });
 
     it("reject transfer ownership", async () => {
+      const { wallet, owner, otherAccount } = await loadFixture(deploy);
 
+      try {
+        await otherAccount.writeContract({
+          address: wallet.address,
+          abi: wallet.abi,
+          functionName: "transferOwnership",
+          args: [otherAccount.account.address]
+        });
+        expect(false).to.be.true;
+      } catch(e: any) {
+        expect(e.details).to.include("OwnableUnauthorizedAccount");
+      }
     });
-  });
 
-  describe("Receiver", () => {
-    it("get receiver", async () => {
+    it("renounce ownership", async () => {
+      const { wallet } = await loadFixture(deploy);
 
+      await wallet.write.renounceOwnership();
+      const addr = await wallet.read.owner();
+
+      expect(addr).to.equal("0x0000000000000000000000000000000000000000");
     });
-  
-    it("set receiver", async () => {
 
-    });
+    it("reject renounce ownership", async () => {
+      const { wallet, owner, otherAccount } = await loadFixture(deploy);
 
-    it("reject set receiver", async () => {
-
+      const addr = wallet.address;
+      try {
+        await otherAccount.writeContract({
+          address: addr,
+          abi: wallet.abi,
+          functionName: "renounceOwnership",
+          args: []
+        });
+        expect(false).to.be.true;
+      } catch(e: any) {
+        expect(e.details).to.include("OwnableUnauthorizedAccount");
+      }
     });
   });
 
   describe("Withdraw", () => {
-    it("allow withdaw", async () => {
-      
-    });
+    // ignore: local network can't execute it
+    // it("allow withdaw", async () => {
+    //  
+    // });
 
     it("reject withdraw", async () => {
+      const { wallet, owner, otherAccount } = await loadFixture(deploy);
+      const to = otherAccount.account.address;
+      const amount = BigInt(1);
 
+      try {
+        await otherAccount.writeContract({
+          address: wallet.address,
+          abi: wallet.abi,
+          functionName: "withdraw",
+          args: [to, amount, to]
+        });
+        expect(false).to.be.true;
+      } catch(e: any) {
+        expect(e.details).to.include("OwnableUnauthorizedAccount");
+      }
     });
   });
 });
